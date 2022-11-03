8ee1c9b0cac38beb69b1e36864a816b6ed378e5e
jasonPonda <jasonvinda@gmail.com>
2022-11-03 19:40:24 +0100

    files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import google from "./assets/google.jpg";
+import code from "./assets/code.jpg";
+import youtube from "./assets/youtube.jpg";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/kinda" element={<Redirect />} />
+        <Route path="/google" element={<Google />} />
+        <Route path="/youtube" element={<Youtube />} />
+      </Routes>
     </div>
   );
 }
+
+//home Page
+const Home = () => {
+  return (
+    <div className="main">
+      <h3>Flux</h3>
+      <div className="container">
+        <div className="card">
+          <img src={code} alt="" />
+          <p>
+            <Link to="/kinda" target="_blank" rel="noreferrer" className="link">
+              Go to KindaCode
+            </Link>
+          </p>
+        </div>
+
+        <div className="card">
+          <img src={google} alt="" />
+          <p>
+            <Link
+              to="/google"
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+            >
+              Go to Google
+            </Link>
+          </p>
+        </div>
+
+        <div className="card">
+          <img src={youtube} alt="" />
+          <p>
+            <Link
+              to="/youtube"
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+            >
+              Go to youtube
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+//Redirect Page
+const Redirect = () => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      window.location.replace("https://www.kindacode.com");
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  // Render some text when redirecting
+  // You can use a loading gif or something like that
+  return (
+    <div className="container">
+      <h3>Redirect...</h3>
+    </div>
+  );
+};
+
+const Google = () => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      window.location.replace("https://www.google.com");
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  // Render some text when redirecting
+  // You can use a loading gif or something like that
+  return (
+    <div className="container">
+      <h3>Redirect...</h3>
+    </div>
+  );
+};
+
+const Youtube = () => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      window.location.replace("https://www.youtube.com");
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  // Render some text when redirecting
+  // You can use a loading gif or something like that
+  return (
+    <div className="container">
+      <h3>Redirect...</h3>
+    </div>
+  );
+};
 
 export default App;
